07e333345dda6dd8eb7ec33e3a313cebf30c9902
Patrick Lee Scott <pat@patscott.io>
2020-04-22 15:27:01 -0400

    fix(keycloak): crds

--- get-crds.sh
+++ get-crds.sh
@@ -1,4 +1,6 @@
 mkdir keycloak-controller
-mkdir -p charts/auth/templates/crds
 curl -L https://github.com/kiwigrid/keycloak-controller/tarball/2.0.0 | tar xz -C keycloak-controller --strip-components=1
-cp keycloak-controller/src/main/k8s/*.yaml charts/auth/templates/crds
+cd keycloak-controller/src/main/k8s
+for filename in *.yaml; do mv "$filename" "0_crd_$filename"; done;
+cd ../../../../
+mv keycloak-controller/src/main/k8s/*.yaml charts/auth/templates
